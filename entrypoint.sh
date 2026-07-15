915788bdf56da694a9b27063a3b0173c4fe9d170
danilaplee <fiend.of.sound@gmail.com>
2026-07-15 16:40:58 +0200

    feat/add-more-configuration-to-docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+envsubst < ./config_template.json > ./config.json
+envsubst < ./config_template.json > ./webapp/config.json
 http-server --proxy "${FFMPEGLAB_HOST-http://localhost:8080/webapp/?}" .
