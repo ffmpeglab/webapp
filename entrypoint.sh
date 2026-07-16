0e5b66dc46eb4a0f0379b91a53c6de43acb9ef26
danilaplee <fiend.of.sound@gmail.com>
2026-07-16 14:58:56 +0200

    feat/fix-env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 envsubst < ./config_template.json > ./config.json
 envsubst < ./config_template.json > ./webapp/config.json
-http-server --proxy "${FFMPEGLAB_HOST}" .
+http-server --proxy "${FFMPEGLAB_HOST}?" .
