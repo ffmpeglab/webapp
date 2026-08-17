b878508ff0a1ce64a7e58e3b7139781ee6e9513f
Daniel <fiend.of.sound@gmail.com>
2026-08-17 04:21:00 +0200

    Feat/better nginx config (#13)
    * feat/update-nginx-upstream
    
    * feat/better-nginx
    
    * feat/better-nginx
    
    * feat/better-nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-envsubst < /usr/share/nginx/html/config_template.json > /usr/share/nginx/html/webapp/config.json
+envsubst < /usr/share/nginx/config_template.json > /usr/share/nginx/html/webapp/config.json
+envsubst < /usr/share/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
+
+cd /usr/share/nginx/html/
+
 nginx -g 'daemon off;'
